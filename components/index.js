5c763aec494d6424e82bf04f7cb2fb12b1686bb2
Susana <susanab20020911@gmail.com>
2023-10-11 16:11:43 -0400

    add home page, not done

---
+++ components/index.js
@@ -0,0 +1,20 @@
+import ScreenHeaderBtn from "./common/header/ScreenHeaderBtn";
+
+// home screen
+import Welcome from "./home/welcome/Welcome";
+
+// common
+import NearbyJobCard from "./common/cards/nearby/NearbyJobCard";
+
+export {
+  ScreenHeaderBtn,
+  Welcome,
+  Nearbyjobs,
+  Popularjobs,
+  Company,
+  JobTabs,
+  JobAbout,
+  JobFooter,
+  Specifics,
+  NearbyJobCard
+};
