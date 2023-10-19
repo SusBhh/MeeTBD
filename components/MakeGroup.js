658d5e8d3cfaa77ddb7563e268175ec435ad72ec
Susana <susanab20020911@gmail.com>
2023-10-19 15:32:12 -0400

    start stylesheet

--- components/MakeGroup.js
+++ components/MakeGroup.js
@@ -4,6 +4,8 @@ import { Button, View, Text, TextInput } from 'react-native';
 import { useState } from "react"
 import { doc, setDoc } from "firebase/firestore";
 import { db } from '../firebaseConfig';
+import { styles } from './../constants/styles'
+// import { COLORS, FONT, SIZES, SHADOWS } from "./theme";
 
 export default function MakeGroup() {
     const [groupName, setGroupName] = useState('');
@@ -19,11 +21,11 @@ export default function MakeGroup() {
     return (
         <View>
             <Text>Make new group here</Text>
-            <TextInput value={groupName} onChangeText={(groupName) => { setGroupName(groupName) }} placeholder="Group Name" style={borderWidth = 0.2}></TextInput>
+            <TextInput value={groupName} onChangeText={(groupName) => { setGroupName(groupName) }} placeholder="Group Name" style={styles.textBoxes}></TextInput>
 
-            <TextInput value={description} onChangeText={(description) => { setDescription(description) }} placeholder="Description"></TextInput>
+            <TextInput value={description} onChangeText={(description) => { setDescription(description) }} placeholder="Description" style={styles.textBoxes}></TextInput>
 
-            <button onClick={create}>Create</button>
+            <button onClick={create} style={styles.buttons}>Create</button>
         </View>
     )
 }
---
+++ constants/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    textBoxes: {
+        borderColor: '#ecdff4',
+        borderWidth: 4,
+        borderRadius: 10,
+        padding: 3,
+        margin: 3
+    },
+    buttons: {
+        color: '#ffffff',
+        backgroundColor: '#ecdff4',
+        borderRadius: 10,
+        padding: 3,
+        margin: 3
+
+    }
+
+})
+export { styles };
