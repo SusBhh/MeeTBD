ea58783920d5c57b5b613e601179c24891624f80
brownzoem <brownzoem@gmail.com>
2023-10-19 10:36:14 -0400

    Add login maybe

---
+++ App.js
@@ -0,0 +1,9 @@
+import "react-native-gesture-handler";
+import * as React from "react";
+
+import LoginScreen from "./navigation/screens/LoginScreen";
+export default function App() {
+    return (
+        <LoginScreen/>
+  );
+}
---
+++ navigation/Menu.js
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+// Screens
+import HomeScreen from './screens/HomeScreen';
+import GroupsScreen from './screens/GroupsScreen';
+import ProfileScreen from './screens/ProfileScreen';
+
+//Screen names
+const homeName = "Home";
+const groupsName = "Groups";
+const profileName = "Profile";
+
+const Tab = createBottomTabNavigator();
+import { NavigationContainer } from '@react-navigation/native';
+function Menu() {
+    return (
+        <NavigationContainer independent={true}>
+            <Tab.Navigator
+                initialRouteName={homeName}
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+                        let rn = route.name;
+
+                        if (rn === homeName) {
+                            iconName = focused ? 'home' : 'home-outline';
+                        } else if (rn === groupsName) {
+                            iconName = focused ? 'list' : 'list-outline';
+                        } else if (rn === profileName) {
+                            iconName = focused ? 'settings' : 'settings-outline';
+                        }
+
+                        // You can return any component that you like here!
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    },
+                    tabBarActiveTintColor: 'tomato',
+                    tabBarInactiveTintColor: 'grey',
+                    tabBarLabelStyle: {
+                        paddingBottom: 10,
+                        fontSize: 10,
+                    },
+                    tabBarStyle: [
+                        {
+                            display: 'flex',
+                        },
+                        null,
+                    ],
+                })}
+            >
+
+                <Tab.Screen name={homeName} component={HomeScreen} />
+                <Tab.Screen name={groupsName} component={GroupsScreen} />
+                <Tab.Screen name={profileName} component={ProfileScreen} />
+
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default Menu;
