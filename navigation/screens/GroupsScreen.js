ac035d34afe3766a2ac6d0f45ac462fefaeb3817
Susana <susanab20020911@gmail.com>
2023-10-19 14:34:53 -0400

    connect database for making group

--- navigation/screens/GroupsScreen.js
+++ navigation/screens/GroupsScreen.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import MakeGroup from './../../components/MakeGroup';
 
 export default function GroupsScreen() {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text
                 style={{ fontSize: 26, fontWeight: 'bold' }}>Groups Screen</Text>
+            <MakeGroup/>
         </View>
     );
 }
